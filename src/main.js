f2dc83bff795a0911e59d220b46ec2bcefddc5b8
Callie Stump <callie@stu.mp>
2020-11-16 16:18:31 -0500

    adds elements to doc. ready
    Co-authored-by: David Sterry <sterry.david@gmail.com>
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,5 +6,18 @@ import "bootstrap";
 $(document).ready(function() {
   $("form#").submit(function(event) {
     event.preventDefault();
+    const userSearch = $("#userSearch").val();
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=l9wbr3917CHeTuV3nddAriQ7oRdk92ra&limit=5`
+
+    request.onreadystatechange = function () {
+      if (this.readystate === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
   });
 });
